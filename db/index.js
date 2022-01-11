a909ae78b05cff5e13844b2e0a586280530e5a1b
YadharthGC <ganeshyadharth@gmail.com>
2022-01-11 08:01:24 +0530

    INITIAL COMMIT

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const mongodb = require("mongodb");
-mongoose.connect("mongodb://localhost:27017/JP", (err) => {
-  if (err) return console.log(error);
-  console.log("connected to mongodb");
-});
+mongoose.connect(
+  "mongodb+srv://ganesh:chitra@cluster0.2pjhw.mongodb.net/JP?retryWrites=true&w=majority",
+  (err) => {
+    if (err) return console.log(error);
+    console.log("connected to mongodb");
+  }
+);
